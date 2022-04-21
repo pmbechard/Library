0807eeb129a66d330a68838d1533e469627216ac
Peyton Bechard <pbechard@my.yorku.ca>
2022-04-21 09:00:49 +0800

    Adds basic modal to 'Add New' button

--- script.js
+++ script.js
@@ -73,7 +73,38 @@ class Book {
 }
 
 
-// For Examples
+// Examples
 const myLib = new Library('Allie\'s Home Library');
 const book1 = new Book('Fluent Python', 'Luciano Ramalho', '2015', 'O\'Reilly', 3, myLib);
 const book2 = new Book('American Psycho', 'Bret Easton Ellis', '1991', 'Random House', 1, myLib);
+
+
+
+
+// MODALS
+
+// Get the modal
+var modal = document.getElementById("add-new-book-modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("add-new-book-button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
