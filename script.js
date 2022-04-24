1f9c2dbd58d57ee42ce2e6a24c528713b92c39db
Peyton Bechard <pbechard@my.yorku.ca>
2022-04-24 11:59:25 +0800

    Adds more exmaples books to work with

--- script.js
+++ script.js
@@ -98,9 +98,18 @@ class Book {
 
 
 // Examples
-const myLib = new Library('Allie\'s Home Library');
-const book1 = new Book('Fluent Python', 'Luciano Ramalho', '2015', 'O\'Reilly', 3, myLib);
-const book2 = new Book('American Psycho', 'Bret Easton Ellis', '1991', 'Random House', 1, myLib);
+const myLib = new Library('Home Library');
+
+const book1 = new Book('Fluent Python', 'Luciano Ramalho', '2015', 'O\'Reilly', 6, myLib);
+const book2 = new Book('American Psycho', 'Bret Easton Ellis', '1991', 'Random House', 4, myLib);
+const book3 = new Book('Python Crash Course, 2nd Ed.', 'Eric Matthes', '2019', 'No Starch Press', 15, myLib);
+const book4 = new Book('Think Like a Programmer: An Introduction to Creative Problem Solving', 'Anton V. Spraul', '2012', 'No Starch Press', 8, myLib);
+const book5 = new Book('Cracking the Coding Interview', 'Gayle Laakmann McDowell', '2015', 'CareerCup', 3, myLib);
+const book6 = new Book('Design Patterns: Elements of Reusable Object-Oriented Software', 'Erich Gamma, Richard Helm, Ralph Johnson, and John Vlissides', '1994', 'Addison-Wesley', 5, myLib);
+const book7 = new Book('Charles Baudelaire: The Flowers of Evil 1868: A New Translation by John E. Tidball', 'John E. Tidball', '2018', 'Bishopston Editions', 3, myLib);
+const book8 = new Book('Glamorama', 'Bret Easton Ellis', '1998', 'Knopf', 4, myLib);
+const book9 = new Book('Trainspotting', 'Irvine Welsh', '1993', 'Secker & Warburg', 5, myLib);
+const book10 = new Book('The Corrections', 'Jonathon Franzen', '2001', 'Farrar, Straus and Giroux', 6, myLib);
 
 
 // SEARCH BAR
